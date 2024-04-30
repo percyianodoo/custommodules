194ed4904d99f5a112f5208278d29ac1bb579dc3
percyianodoo <95726928+percyianodoo@users.noreply.github.com>
2024-04-30 13:53:15 +0800

    Update __manifest__.py
    i added test

--- account_payment_mode/__manifest__.py
+++ account_payment_mode/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2020 Akretion France (<https://www.akretion.com>)
+# Copyright 2016-2020 Akretion France (<https://www.akretion.com>)test
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
